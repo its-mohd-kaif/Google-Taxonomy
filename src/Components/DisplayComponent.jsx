06e2f69cf403809ed5995d6f5e7e4bb92e81bd34
its-mohd-kaif <mohdkaif@cedcoss.com>
2023-02-09 15:34:12 +0530

    Done

---
+++ src/Components/DisplayComponent.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+function DisplayComponent({ data, index, makeSubCategory }) {
+  // Display List Component
+  return (
+    <div>
+      <div style={{ width: "50%" }}>
+        {/* Check Validation */}
+        {Object.keys(data).length !== 0 ? (
+          <select
+            onChange={(e) => {
+              makeSubCategory(data, e.target.value, index);
+            }}
+            className="form-select"
+            aria-label="Default select example"
+          >
+            <option disabled selected>
+              --Select--
+            </option>
+            {/* Map */}
+            {Object.keys(data).map((val, index) => (
+              <option key={index} value={val}>
+                {val}
+              </option>
+            ))}
+          </select>
+        ) : null}
+      </div>
+    </div>
+  );
+}
+
+export default DisplayComponent;
--- src/Components/LandingPage.jsx
+++ src/Components/LandingPage.jsx
@@ -1,128 +1,55 @@
 import React, { useEffect, useState } from "react";
 import "../App.css";
+import DisplayComponent from "./DisplayComponent";
 const file = require("../Data/file.txt");
 function LandingPage() {
-  const [data, setData] = useState([]);
-  const [cate, setCate] = useState([]);
-  const [select1, setSelect1] = useState("");
-  const [sub, setSub] = useState([]);
-  const [str, setStr] = useState([]);
+  // UseState For List Array
+  const [list, setList] = useState([]);
+  // Index Array
+  const [indexArr, setIndexArr] = useState([]);
+  let obj = {};
+  let values = [];
   useEffect(() => {
     fetch(file)
       .then((res) => res.text())
       .then((val) => {
-        setData(val.split("\n"));
-        let temp = [];
-        val.split("\n").map((ele) => {
-          if (ele.includes(">") === false && ele !== "") {
-            temp.push(ele);
-          }
+        let arr = val.split("\n");
+        arr.map((ele) => {
+          values = ele.split(" > ");
+          // Call Method that make data into key value form
+          createNestedObject(obj, values);
+          setList([...list, obj]);
         });
-        setCate(temp);
       });
-  }, [sub, str]);
-  const mainSelectHandler = (e) => {
-    let tempSelect = [];
-    console.log(data);
-    setSelect1(e.target.value);
-    let unique = makeDropDown(e.target.value);
-    setSub(unique);
-    tempSelect.push(
-      <select
-        onChange={subSelectHandler}
-        className="form-select"
-        aria-label="Default select example"
-      >
-        <option>--Select--</option>
-        {unique.map((val, index) => (
-          <option key={index} value={val}>
-            {val}
-          </option>
-        ))}
-      </select>
-    );
-    setStr([...str, tempSelect]);
+  }, []);
+  // Function that make nested key value objects
+  var createNestedObject = function (object, array) {
+    for (var i = 0; i < array.length; i++) {
+      object = object[array[i]] = object[array[i]] || {};
+    }
   };
-  const subSelectHandler = (e) => {
-    console.log(e.target.value);
-    let temp = [];
-    for (let i = 0; i < data.length; i++) {
-      if (data[i].includes(e.target.value) === true) {
-        temp.push(data[i]);
-      }
+
+  const makeSubCategory = (data, value, index) => {
+    if (indexArr.includes(index)) {
+      list.splice(index + 1, list.length - index, data[value]);
+      setList([...list]);
+    } else {
+      setList([...list, data[value]]);
+      setIndexArr([...indexArr, index]);
     }
-    let filter = [];
-    temp.map((val) => {
-      val.split(" > ").map((item) => {
-        filter.push(item);
-      });
-    });
-    let final = [];
-    for (let i = 0; i < filter.length; i++) {
-      if (filter[i] === e.target.value && filter[i + 1] !== "") {
-        final.push(filter[i + 1]);
-      }
-    }
-    let unique = [...new Set(final)];
-    let tempSelect = [];
-    tempSelect.push(
-      <select
-        // onChange={subSelectHandler}
-        className="form-select"
-        aria-label="Default select example"
-      >
-        <option>--Select--</option>
-        {unique.map((val, index) => (
-          <option key={index} value={val}>
-            {val}
-          </option>
-        ))}
-      </select>
-    );
-    setStr([...str, tempSelect]);
-    console.log("SUB", unique);
   };
-  function makeDropDown(para) {
-    let temp = [];
-    data.map((val) => {
-      if (val.startsWith(`${para} >`) === true) {
-        temp.push(val);
-      }
-    });
-    let filter = [];
-    temp.map((val) => {
-      val.split(" > ").map((item) => {
-        filter.push(item);
-      });
-    });
-    let final = [];
-    for (let i = 0; i < filter.length; i++) {
-      if (filter[i] === para && filter[i + 1] !== "") {
-        final.push(filter[i + 1]);
-      }
-    }
-    let unique = [...new Set(final)];
-    return unique;
-  }
   return (
     <div>
       <center>
         <h1>Google Taxonomy</h1>
-        <div style={{ width: "50%" }}>
-          <select
-            onChange={mainSelectHandler}
-            className="form-select"
-            aria-label="Default select example"
-          >
-            <option>--Select--</option>
-            {cate.map((val, index) => (
-              <option key={index} value={val}>
-                {val}
-              </option>
-            ))}
-          </select>
-          {str.length !== undefined ? str : ""}
-        </div>
+        {list.map((val, index) => (
+          <DisplayComponent
+            key={index}
+            data={val}
+            index={index}
+            makeSubCategory={makeSubCategory}
+          />
+        ))}
       </center>
     </div>
   );
